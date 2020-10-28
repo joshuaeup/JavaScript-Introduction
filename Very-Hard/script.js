74ebef1d43a8cc17be3baba6569dd4c5f7c8dc3f
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2020-10-27 21:27:57 -0400

    Added front end to Calculator

--- Very-Hard/script.js
+++ Very-Hard/script.js
@@ -1,34 +1,81 @@
-var numOne = prompt("Please enter the first number");
-var operator = prompt("Please enter the operator");
-var numTwo = prompt("Please enter the second number");
-// passes integer value into calculating funtion
-calculate(parseInt(numOne), operator, parseInt(numTwo));
+// var numOne = prompt("Please enter the first number");
+// var operator = prompt("Please enter the operator");
+// var numTwo = prompt("Please enter the second number");
 
-function calculate(numOne, operator, numTwo) {
+var operator = "";
+var addIcon = document.getElementById("add");
+var subtractIcon = document.getElementById("subtract");
+var multiplyIcon = document.getElementById("multiply");
+var divideIcon = document.getElementById("divide");
+
+function calculate() {
+    var numOne = parseFloat(document.getElementById("numOne").value);
+    var numTwo = parseFloat(document.getElementById("numTwo").value);
+
+    
+    
+
     switch(operator) {
         case "+":
+            
             // If operator is add then call add function
-            console.log(numOne + " " + operator + " " + numTwo + " = " +  add(numOne, numTwo));
+            console.log(numOne + " " + operator + " " + numTwo + " = " +  add(numOne, numTwo).toFixed(2));
+            document.getElementById("calculateSubTitle").innerHTML = add(numOne, numTwo).toFixed(2);
             break;
             
         case "-":
             // If operator is subtract then call subtract function
-            console.log(numOne + " " + operator + " " + numTwo + " = " + subtract(numOne, numTwo));
+            console.log(numOne + " " + operator + " " + numTwo + " = " + subtract(numOne, numTwo).toFixed(2));
+            document.getElementById("calculateSubTitle").innerHTML = subtract(numOne, numTwo).toFixed(2);
             break;
     
         case "*":
             // If operator is multiply then call add multiply
-            console.log(numOne + " " + operator + " " + numTwo + " = " + multiply(numOne, numTwo));
+            console.log(numOne + " " + operator + " " + numTwo + " = " + multiply(numOne, numTwo).toFixed(2));
+            document.getElementById("calculateSubTitle").innerHTML = multiply(numOne, numTwo).toFixed(2);
             break;
             
         case "/":
             // If operator is divide then call divide function
-            console.log(numOne + " " + operator + " " + numTwo + " = " + divide(numOne, numTwo));
+            console.log(numOne + " " + operator + " " + numTwo + " = " + divide(numOne, numTwo).toFixed(2));
+            document.getElementById("calculateSubTitle").innerHTML = divide(numOne, numTwo).toFixed(2);
             break;
     
         default:
             console.log("I'm sorry that is not a valid operator");
     }
+}
+
+function addOperator() {
+    addIcon.style.color = "#EF8441";
+    subtractIcon.style.color = "#4FE782";
+    multiplyIcon.style.color = "#4FE782";
+    divideIcon.style.color = "#4FE782";
+    operator = "+";
+}
+
+function subtractOperator() {
+    addIcon.style.color = "#4FE782";
+    subtractIcon.style.color = "#EF8441";
+    multiplyIcon.style.color = "#4FE782";
+    divideIcon.style.color = "#4FE782";
+    operator = "-";
+}
+
+function multiplyOperator() {
+    addIcon.style.color = "#4FE782";
+    subtractIcon.style.color = "#4FE782";
+    multiplyIcon.style.color = "#EF8441";
+    divideIcon.style.color = "#4FE782";
+    operator = "*";
+}
+
+function divideOperator() {
+    addIcon.style.color = "#4FE782";
+    subtractIcon.style.color = "#4FE782";
+    multiplyIcon.style.color = "#4FE782";
+    divideIcon.style.color = "#EF8441";
+    operator = "/";
 }
 
 
@@ -47,3 +94,6 @@ function multiply(numOne, numTwo) {
 function divide(numOne, numTwo) {
     return numOne / numTwo;
 }
+
+
+
